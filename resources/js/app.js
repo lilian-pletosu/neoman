9e883a80c6013092babcf763079faf89a131a8fb
Pletosu Lilian <lilian.pletosu@neobytesolutions.com>
2023-10-18 00:37:15 +0300

    repair problems with imports in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,9 @@
 import './bootstrap';
 import '../css/app.css';
-import {Animate, initTE,} from "tw-elements";
 import {createApp, h} from 'vue';
 import {createInertiaApp} from '@inertiajs/vue3';
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
-
-initTE({Animate});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Neoman';
 
