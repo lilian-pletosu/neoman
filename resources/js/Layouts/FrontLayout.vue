998493e685e9ac69b0dde39082e807708b2f4916
Lilian Pletosu <lilianpletosu7@gmail.com>
2024-05-23 22:25:07 +0300

    new

--- resources/js/Layouts/FrontLayout.vue
+++ resources/js/Layouts/FrontLayout.vue
@@ -80,7 +80,7 @@ onMounted(() => {
         <body class=" dark:bg-dark overflow-x-hidden">
         <Head :title="title">
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <meta name="author" content="Neoman">
 
