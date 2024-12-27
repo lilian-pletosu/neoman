141f7f9952f1e495828da738627767926724b16b
“Lilian <lilian.pletosu@trakosoft.ro>
2024-12-27 10:25:43 +0200

    `Removed commented out routes in auth.php`

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-//    Route::get('register', [RegisteredUserController::class, 'create'])
-//                ->name('register');
-//
-//    Route::post('register', [RegisteredUserController::class, 'store']);
+    //    Route::get('register', [RegisteredUserController::class, 'create'])
+    //                ->name('register');
+    //
+    //    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -46,8 +46,8 @@ Route::middleware('auth')->group(function () {
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+    //     ->name('password.confirm');
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
