275aa9328f8e9ad9f379c98fba08fd0cd4026fba
“Lilian <lilian.pletosu@trakosoft.ro>
2025-01-27 10:15:59 +0200

    Refactor CarouselFront component for improved readability and add lazy loading to images

--- resources/js/Components/CarouselFront.vue
+++ resources/js/Components/CarouselFront.vue
@@ -1,31 +1,41 @@
 <script setup>
-
-import 'vue3-carousel/dist/carousel.css'
-import {Carousel, Navigation, Slide} from 'vue3-carousel'
-import {getCurrentInstance} from "vue";
-import {Link} from "@inertiajs/vue3";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Navigation, Slide } from "vue3-carousel";
+import { getCurrentInstance } from "vue";
+import { Link } from "@inertiajs/vue3";
 
 const app = getCurrentInstance();
 </script>
 
 <template>
-    <carousel :autoplay="5000" :wrap-around="true" :items-to-show="1" class="w-screen z-0"
-              :transition="500">
-        <slide v-for="(slide, index) in app.appContext.config.globalProperties.$page.props.home_banners" :key="index">
+    <carousel
+        :autoplay="5000"
+        :wrap-around="true"
+        :items-to-show="1"
+        class="z-0 w-screen"
+        :transition="500"
+    >
+        <slide
+            v-for="(slide, index) in app.appContext.config.globalProperties
+                .$page.props.home_banners"
+            :key="index"
+        >
             <Link :href="slide.link">
-                <img class="object-cover w-screen flex"
-                     :src="slide.image" :alt="slide.title">
+                <img
+                    class="flex object-cover w-screen"
+                    loading="lazy"
+                    :src="slide.image"
+                    :alt="slide.title"
+                />
             </Link>
         </slide>
         <template #addons>
-            <Navigation/>
-            <Pagination/>
+            <Navigation />
+            <Pagination />
         </template>
-
     </carousel>
-
-
-</template>-
+</template>
+-
 
 <style scoped>
 .no-padding {
